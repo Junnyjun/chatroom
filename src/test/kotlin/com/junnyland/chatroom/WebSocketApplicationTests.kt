4c2a2cbe675d7cf415020fce52e0a501aa670ab9
junnyJun <chbe5082@naver.com>
2023-04-06 17:11:12 +0900

    init

---
+++ src/test/kotlin/com/junnyland/chatroom/WebSocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.junnyland.chatroom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebSocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
