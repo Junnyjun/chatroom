2dac21ca015c7e17d0924a00181b02596480e4e6
junnyJun <chbe5082@naver.com>
2023-04-10 14:36:09 +0900

    feat (chat) : binder

--- src/main/kotlin/com/junnyland/play/chatroom/gateway/in/view/WebBinder.kt
+++ src/main/kotlin/com/junnyland/play/chatroom/gateway/in/view/WebBinder.kt
@@ -1,8 +1,12 @@
 package com.junnyland.play.chatroom.gateway.`in`.view
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@EnableWebMvc
+@Configuration
 class WebBinder: WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addViewController("/").setViewName("forward:/index.html")
