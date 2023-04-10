c26c09c79bd5f206b6fafda0bb137005a7e5111c
junnyJun <chbe5082@naver.com>
2023-04-10 14:36:28 +0900

    feat (chat) : socket -> stomp

--- src/main/kotlin/com/junnyland/play/config/socket/WebSocketConfig.kt
+++ src/main/kotlin/com/junnyland/play/config/socket/WebSocketConfig.kt
@@ -1,18 +1,25 @@
 package com.junnyland.play.config.socket
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.WebSocketHandler
-import org.springframework.web.socket.config.annotation.EnableWebSocket
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 class WebSocketConfig(
-    private val socketHandler: WebSocketHandler
-) : WebSocketConfigurer {
-    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(socketHandler, "/ws/chat")
+    private val socketHandler: WebSocketHandler,
+) : WebSocketMessageBrokerConfigurer {
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/topic")
+        registry.setApplicationDestinationPrefixes("/kafka")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws-chat")
             .setAllowedOrigins("*")
     }
 }
