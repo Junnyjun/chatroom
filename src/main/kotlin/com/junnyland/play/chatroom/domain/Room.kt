046be519058af0ee4e4a902940f1a4008fe63a7a
junnyJun <chbe5082@naver.com>
2023-04-10 14:37:02 +0900

    feat (chat) : message sender

--- src/main/kotlin/com/junnyland/play/chatroom/domain/Room.kt
+++ src/main/kotlin/com/junnyland/play/chatroom/domain/Room.kt
@@ -4,6 +4,11 @@ data class Room(
     val id: String,
     val name: String,
     val description: String,
+    val status:Status
 ){
-    constructor(name: String, description: String) : this("", name, description)
+    constructor(name: String, description: String) : this("", name, description, Status.OPEN)
+
+    enum class Status {
+        OPEN, CLOSE
+    }
 }
--- src/main/kotlin/com/junnyland/play/chatroom/gateway/out/message/Sender.kt
+++ src/main/kotlin/com/junnyland/play/chatroom/gateway/out/message/Sender.kt
@@ -1,19 +1,27 @@
 package com.junnyland.play.chatroom.gateway.out.message
 
 import com.junnyland.play.chatroom.domain.Message
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.stereotype.Component
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.SimpMessageSendingOperations
+import org.springframework.stereotype.Controller
+
+private const val TOPIC = "chatroom"
 
 interface Sender {
     fun send(message: Message)
 
-    @Component
+    @Controller
     class KafkaSender(
-        private val kafkaTemplate: KafkaTemplate<String, String>,
-    ): Sender {
+        private val sender: SimpMessageSendingOperations,
+    ) : Sender {
+        @MessageMapping("/chat/send")
         override fun send(message: Message) {
-            println("KafkaSender: $message")
-            kafkaTemplate.send(message.target,message.message)
+            val target: String = "/topic/${message.roomId}"
+
+            if (Message.MessageType.JOIN == message.type) {
+                sender.convertAndSend(target, "${message.sender} joined")
+            }
+            sender.convertAndSend("/topic/${message.roomId}", message)
         }
     }
 }
