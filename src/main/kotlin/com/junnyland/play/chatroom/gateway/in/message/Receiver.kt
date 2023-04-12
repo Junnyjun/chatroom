ca187cb2ff80c2eb92be24ceb783763f7e3d8de2
junnyJun <chbe5082@naver.com>
2023-04-12 16:57:37 +0900

    feat (chat) : receiver message

--- src/main/kotlin/com/junnyland/play/chatroom/gateway/in/message/Receiver.kt
+++ src/main/kotlin/com/junnyland/play/chatroom/gateway/in/message/Receiver.kt
@@ -1,28 +1,27 @@
 package com.junnyland.play.chatroom.gateway.`in`.message
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.junnyland.play.chatroom.domain.Message
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
 
 
 interface Receiver {
     fun enter(message: Message)
 
-    @Controller
+    @Component
     class KafkaReceiver(
         private val template: SimpMessagingTemplate,
-        @Value("\${chatroom.path}") private val path: String,
     ) : Receiver {
 
-        @KafkaListener(topics = ["chatroom"], groupId = "junnyland", containerFactory = "kafkaListenerContainerFactory")
-        override fun enter(
-            message: Message,
-        ) {
-            println("KafkaConsumerConfig: $message")
-            template.convertAndSend(path, message)
+        @KafkaListener(
+            topics = ["chatroom"],
+            groupId = "junnyland",
+            containerFactory = "kafkaListenerContainerFactory"
+        )
+        override fun enter(message: Message, ) {
+            template.convertAndSend("/topic/group", message)
         }
     }
 }
