1bf099d534da3a3c6103a912017b4bac6aaff999
junnyJun <chbe5082@naver.com>
2023-04-12 13:07:05 +0900

    feat (chat) : message

--- src/main/kotlin/com/junnyland/play/chatroom/domain/Message.kt
+++ src/main/kotlin/com/junnyland/play/chatroom/domain/Message.kt
@@ -1,10 +1,14 @@
 package com.junnyland.play.chatroom.domain
 
+import java.time.LocalDateTime
+import java.time.LocalDateTime.now
+
 data class Message(
     val message: String,
     val roomId: String,
     val sender: String,
     val type: MessageType,
+    val timestamp: String = now().toString()
 ){
     enum class MessageType {
         CHAT,
@@ -12,3 +16,4 @@ data class Message(
         LEAVE
     }
 }
+
