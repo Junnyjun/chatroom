6e8b47d5f1c84fe28f8fe52ed9a0e83666ebc543
junnyJun <chbe5082@naver.com>
2023-04-12 16:55:35 +0900

    feat (chat) : add createAT

--- src/main/kotlin/com/junnyland/play/chatroom/gateway/out/repository/ChatRoom.kt
+++ src/main/kotlin/com/junnyland/play/chatroom/gateway/out/repository/ChatRoom.kt
@@ -2,25 +2,33 @@ package com.junnyland.play.chatroom.gateway.out.repository
 
 import com.junnyland.play.chatroom.domain.Room
 import com.junnyland.play.chatroom.domain.Room.Status.OPEN
+import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.IndexDirection
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
 
 @Document(collection = "chatRoom")
 class ChatRoom(
-    @Id private val id: String = "",
+    @Id private val id: ObjectId = ObjectId.get(),
     @Indexed(unique = true) private val name: String,
-    private var description: String = "",
+    private val description: String = "",
     private val status: Room.Status = OPEN,
+    @Indexed(direction = IndexDirection.DESCENDING) private val createdAt: LocalDateTime = LocalDateTime.now(),
 ) {
 
     fun toDomain(): Room {
-        return Room(id, name, description, status)
+        return Room(id.toString(), name, description, status)
     }
 
     companion object {
         fun byDomain(room: Room): ChatRoom {
-            return ChatRoom(room.name, room.description)
+            return ChatRoom(
+                name = room.name,
+                description = room.description,
+                status = room.status
+            )
         }
     }
 }
